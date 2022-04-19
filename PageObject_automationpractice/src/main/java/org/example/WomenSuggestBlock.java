82f49f7023419acdc35561434f59815d4ebf1d8c
AleksandrMelekhin <85239026+AleksandrMelekhin@users.noreply.github.com>
2022-04-19 18:50:07 +0300

    Added some pages

---
+++ PageObject_automationpractice/src/main/java/org/example/WomenSuggestBlock.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class WomenSuggestBlock extends BaseView {
+    public WomenSuggestBlock(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//ul[contains(@class, 'sf-menu')]/li/a[@title='T-shirts']")
+    private WebElement tShirtsButton;
+
+    @Step("Кликнуть на кнопку 'Футболки'")
+    public TShirtsPage tShirtsButtonClick() {
+        tShirtsButton.click();
+        return new TShirtsPage(driver);
+    }
+}
+
